ec1ec43d03acb62f19ff81a506e696b27d41e2bf
birdortyedi <furkan.kinli@ozu.edu.tr>
2022-03-10 22:05:40 +0300

    weights link updated

--- models/download.py
+++ models/download.py
@@ -2,8 +2,7 @@ import requests
 
 
 def download_from(url, filepath):
-    headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
-    r = requests.get(url, stream=True, headers=headers)
+    r = requests.get(url, stream=True)
     with open(filepath, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
@@ -11,7 +10,7 @@ def download_from(url, filepath):
 
 
 if __name__ == '__main__':
-    model_64_dst_url = "https://www.dropbox.com/s/sk7pybq3xrnithc/ifrnet_p_64_D_S_T.pth?dl=0"
-    model_128_dst_url = "https://www.dropbox.com/s/f1tthna0hri59l6/ifrnet_p_128_D_S_T.pth?dl=0"
+    model_64_dst_url = "https://www.dropbox.com/s/m1on38qcsmo1768/ifrnet_p_64_D_S_T.pth?dl=1"
+    model_128_dst_url = "https://www.dropbox.com/s/f1tthna0hri59l6/ifrnet_p_128_D_S_T.pth?dl=1"
     download_from(model_64_dst_url, "models/ifrnet_p_64_D_S_T.pth")
     download_from(model_128_dst_url, "models/ifrnet_p_128_D_S_T.pth")
